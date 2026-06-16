623373ee53e1711507e5e6beb8697d90ff073933
DoogieMuc <info@liquido.vote>
2026-06-16 23:38:45 +0200

    Updated deployToGismo.sh script

--- deploy/deployToGismo.sh
+++ deploy/deployToGismo.sh
@@ -7,25 +7,54 @@ set -e
 
 # TODO: build backend  "mvn package" with tests
 
-echo "Copy LIQUIDO quarkus-app to gismo:/opt/liquido-on-gismo"
+printf "Do you want to build the backend [Y/n/t] "
+read build_backend
+case "$build_backend" in
+	[tT])
+		printf "Building LIQUIDO backend WITH tests... "
+		if ./mvnw clean package; then
+      echo "✅ Backend build and tested successfully.\n"
+    else
+      echo "❌ Backend build failed.\n"
+      exit 1
+    fi
+		;;
+  [nN])
+    echo "Won't build."
+    ;;
+	*)
+		printf "Building LIQUIDO backend WITHOUT tests ... "
+		if ./mvnw clean package -Dmaven.test.skip; then
+      echo "✅ Backend build successfully. (WITHOUT running any tests.)\n"
+    else
+      echo "❌ Backend build failed.\n"
+      exit 1
+    fi
+		;;
 
-rsync --recursive --delete-after ./target/quarkus-app/ gismo:/opt/liquido-on-gismo/
-
-rsync --ignore-times ./config/application-gismo.properties gismo:/opt/liquido-on-gismo/config/
+esac
 
 echo
-printf "Do you want to restart LIQUIDO on GISMO? [y/N] "
+echo "Copy LIQUIDO quarkus-app to gismo:/opt/liquido-on-gismo"
+rsync --recursive --delete-after ./target/quarkus-app/ gismo:/opt/liquido-on-gismo/
+rsync --progress --ignore-times ./config/application-gismo.properties gismo:/opt/liquido-on-gismo/config/
+
+echo
+printf "Do you want to restart LIQUIDO on GISMO? [Y/n] "
 read restart_liquido
 
 case "$restart_liquido" in
-	[yY]|[yY][eE][sS])
+  [nN])
+      echo "Won't restart."
+      ;;
+	*)
 		printf "Restarting LIQUIDO on GISMO: "
 		ssh gismo 'sudo /bin/systemctl restart liquido'
 		if ssh gismo 'sudo /bin/systemctl is-active --quiet liquido'; then
-			echo "✅"
+			echo " => Backend restarted successfully ✅"
 		else
 			status_output="$(ssh gismo 'sudo /bin/systemctl status liquido --no-pager' || true)"
-			echo "LIQUIDO service is not active (running)"
+			echo "❌ LIQUIDO service is not active (running)"
 			echo "$status_output"
 			exit 1
 		fi
@@ -46,18 +75,17 @@ while [ "$check_attempt" -le "$max_attempts" ]; do
   fi
   response="$(curl -k --silent --show-error $LIQUIDO_API_URL || true)"
   if printf '%s' "$response" | grep -q 'LIQUIDO'; then
-    echo " => Backend is alive. ✅"
+    echo " => ✅ Backend is alive."
     break
   fi
   if [ "$check_attempt" -eq "$max_attempts" ]; then
-    echo " => FAILED! (${check_attempt}/${max_attempts}): $response"
+    echo " => ❌ FAILED! (${check_attempt}/${max_attempts}): $response"
     exit 1
   fi
   check_attempt=$((check_attempt + 1))
   sleep 1
 done
 
-echo
 printf "Checking frontend at $LIQUIDO_FRONTEND_URL"
 response="$(curl -k --silent --show-error $LIQUIDO_FRONTEND_URL || true)"
 if printf '%s' "$response" | grep -qi '^<html' \
